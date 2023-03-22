5d1dd00c350de3af1397b1b1983bf5c75f52dbe5
Tester Tester <romanpodgornykh@gmail.com>
2023-03-22 19:08:30 +0200

    220323

--- src/main/java/SampleTest.java
+++ src/main/java/SampleTest.java
@@ -1,13 +1,18 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SampleTest {
     public static void main(String[] args)  {
-System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\IdeaProjects\\TestAutomation\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\roman\\IdeaProjects\\TestAutomation\\chromedriver.exe");
 
-        driver.quit();
+        ChromeDriver driver = new ChromeDriver(co);
+        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[1]/button")).click();
+
     }
 }
 
